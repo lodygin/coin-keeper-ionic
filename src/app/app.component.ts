f8bd74a0a13d3005058cd3d2b092ffd42a9d6871
Sasha <savylovskij@gmail.com>
2023-05-27 22:47:42 +0300

    chore(initial): issue #6. Update eslint
    Close #6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
-  standalone: true,
-  imports: [IonicModule],
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss'],
+    standalone: true,
+    imports: [IonicModule],
 })
-export class AppComponent {
-  constructor() {}
-}
+export class AppComponent {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,12 +2,10 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: true,
-  imports: [IonicModule],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
+    standalone: true,
+    imports: [IonicModule],
 })
-export class HomePage {
-  constructor() {}
-}
+export class HomePage {}
