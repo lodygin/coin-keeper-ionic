e93ac5a589059949208f977339ea7a07e57ecf8f
lodygin <lodigin.alexander@gmail.com>
2023-05-28 21:33:09 +0300

    chore: test ci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss'],
-    standalone: true,
-    imports: [IonicModule],
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+  standalone: true,
+  imports: [IonicModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor() {
+    console.log('AppComponent Init');
+  }
+}
